81d617a7d6411c6fdf09c0f008ef5bfd93cf4752
sergio.pena <sergio.pena.orozco@gmail.com>
2025-01-23 23:41:59 -0600

    add config for connect to mongo db

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import server from './server';
+import colors from 'colors';
 
 const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
-    console.log(`UPTASK API running on port ${port}`);
+    console.log(colors.cyan.bold(`UPTASK API running on port ${port}`));
 })
