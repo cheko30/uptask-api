db624989aa2926a603eb5c2af7cab239a316b4a7
sergio.pena <sergio.pena.orozco@gmail.com>
2025-01-27 22:27:26 -0600

    add middleware validation for create a new project

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db';
+import projectsRoutes from './routes/projectRoutes';
 
+// Load environment variables
 dotenv.config();
+
+// Connection to database
 connectDB();
 
+// Create an Express application
 const app = express();
+
+// Config to receive JSON data
+app.use(express.json());
+
+// Routes
+app.use('/api/projects', projectsRoutes);
 
 export default app;
