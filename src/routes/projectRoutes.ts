b27101dd125ae93ed0cfea6fdcc01fd8ae9607b5
sergio.pena <sergio.pena.orozco@gmail.com>
2025-01-28 23:23:57 -0600

    add service for add task to one project

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import { body, param } from 'express-validator';
 import { ProjectController } from '../controllers/ProjectController';
 import { handleInputErrors } from '../middleware/validation';
+import { TaskController } from '../controllers/TaskController';
+import { validateProjectExists } from '../middleware/project';
 
 const router = Router();
 
@@ -34,6 +36,15 @@ router.delete('/:id',
     param('id').isMongoId().withMessage('Invalid ID'),
     handleInputErrors,
     ProjectController.deleteProject
+);
+
+/** Routes for task */
+router.post('/:projectId/task',
+    validateProjectExists,
+    body('name').notEmpty().withMessage('Task name is required'),
+    body('description').notEmpty().withMessage('The description task is required'),
+    handleInputErrors,
+    TaskController.createTask
 );
 
 export default router;
