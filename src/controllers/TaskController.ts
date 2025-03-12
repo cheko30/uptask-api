54513bc1b64dbb29c2bcbca5b240bdb8af6f47e3
sergio.pena <sergio.pena.orozco@gmail.com>
2025-03-11 23:32:34 -0600

    add completedBy task

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -28,7 +28,9 @@ export class TaskController {
 
     static getTaskById = async (req: Request, res: Response) => {
         try {
-            res.json(req.task);
+            const task = await Task.findById(req.task.id)
+                                    .populate({path: 'completedBy.user', select: 'id name email'})
+            res.json(task);
         } catch (error) {
             res.status(500).json({ error: 'An error ocurred' });
         }
@@ -61,6 +63,13 @@ export class TaskController {
         try {
             const { status } = req.body;
             req.task.status = status;
+
+            const data = {
+                user: req.user.id,
+                status
+            }
+            
+            req.task.completedBy.push(data)
             await req.task.save();
 
             res.send('Status task updated successfully');
